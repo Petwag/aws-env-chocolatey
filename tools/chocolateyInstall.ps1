a62028d5c1b7e6d31d963f9b7110ae0eb2cef371
Petwag <fhortet.petwag@gmail.com>
2026-05-14 22:06:16 +0100

    fix(install): change the ps1 file logic to use exe as an exe not an installer

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,15 +1,18 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'aws-env'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $url64 = 'https://github.com/Petwag/aws-env/releases/download/v{{VERSION}}/aws-env.exe'
-
 $checksum64 = '{{CHECKSUM}}'
 
-Install-ChocolateyPackage `
+Get-ChocolateyWebFile `
   -PackageName $packageName `
-  -FileType 'exe' `
-  -SilentArgs '/S' `
+  -FileFullPath "$toolsDir\aws-env.exe" `
   -Url64bit $url64 `
   -Checksum64 $checksum64 `
   -ChecksumType64 'sha256'
+
+Install-BinFile `
+  -Name 'aws-env' `
+  -Path "$toolsDir\aws-env.exe"
